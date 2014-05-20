18dbda11f1a2cb1fcbe11a05be2f622848191eb7
Abion47 <andrew.ackie@gmail.com>
2014-05-20 00:49:27 -0600

    Add TFFTableViewer

---
+++ C#/TTFTableViewer/Conversions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTFTableViewer
+{
+    public class Conversions
+    {
+        static float ByteArrayToFixed(byte[] arr)
+        {
+            short mantissa = ByteArrayToShort(arr, 0);
+            short fraction = ByteArrayToShort(arr, 2);
+
+            return (float)mantissa + ((float)fraction / 16384);
+        }
+
+        static float ByteArrayToF2Dot14(byte[] arr)
+        {
+            int mantissa = arr[0] >> 6;
+
+            int fraction = arr[0] & 0x3F;
+            fraction = fraction << 8;
+            fraction += arr[1];
+
+            return (float)mantissa + ((float)fraction / 16384);
+        }
+
+        static short ByteArrayToShort(byte[] arr, int startIndex = 0)
+        {
+            short sign = (arr[0] >> 7) == 0 ? (short)1 : (short)-1;
+            short val = (short)(arr[startIndex] & 0x7F);
+            val = (short)(val << 8);
+            val += (short)arr[startIndex + 1];
+            return (short)(val * sign);
+        }
+
+        static ushort ByteArrayToUnsignedShort(byte[] arr, int startIndex = 0)
+        {
+            ushort val = (ushort)arr[startIndex];
+            val = (ushort)(val << 8);
+            val += (ushort)arr[startIndex + 1];
+            return val;
+        }
+
+        static int ByteArrayToInt(byte[] arr, int startIndex = 0)
+        {
+            int sign = (arr[0] >> 7) == 0 ? 1 : -1;
+            int val = arr[startIndex] & 0x7F;
+            val = val << 8;
+            val += arr[startIndex + 1];
+            val = val << 8;
+            val += arr[startIndex + 2];
+            val = val << 8;
+            val += arr[startIndex + 3];
+            return val * sign;
+        }
+
+        static uint ByteArrayToUnsignedInt(byte[] arr, int startIndex = 0)
+        {
+            uint val = arr[startIndex];
+            val = val << 8;
+            val += arr[startIndex + 1];
+            val = val << 8;
+            val += arr[startIndex + 2];
+            val = val << 8;
+            val += arr[startIndex + 3];
+            return val ;
+        }
+
+        // ==================== To String =========================
+
+        static string ByteToBinaryString(byte b)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte i = b;
+
+            while (i > 0)
+            {
+                sb.Insert(0, i & 1);
+                i = (byte)(i >> 1);
+            }
+
+            while (sb.Length < 8)
+                sb.Insert(0, 0);
+
+            return sb.ToString();
+        }
+
+        static string IntToBinaryString(int b)
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = b;
+
+            while (i > 0)
+            {
+                sb.Insert(0, i & 1);
+                i = (byte)(i >> 1);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#/TTFTableViewer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTFTableViewer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());*/
+
+            string path = @"F:\Users\Abion47\Projects\XNA\Samples\RedistributableTTFs_ARCHIVE_3_1\RedistributableTTFs\SegoeUIMono-Regular.ttf";
+            ParseTTF(path);
+
+            Console.WriteLine("test");
+            Console.ReadKey();
+        }
+
+        static void ParseTTF(string path)
+        {
+            FileStream stream = File.OpenRead(path);
+            byte[] fileContents = new byte[stream.Length];
+            stream.Read(fileContents, 0, fileContents.Length);
+            stream.Close();
+
+            byte[] versionBin = new byte[4];
+            byte[] numTablesBin = new byte[2];
+            byte[] searchRangeBin = new byte[2];
+            byte[] entrySelectorBin = new byte[2];
+            byte[] rangeShiftBin = new byte[2];
+
+            Array.Copy(fileContents, 0, versionBin, 0, versionBin.Length);
+            Array.Copy(fileContents, 4, numTablesBin, 0, numTablesBin.Length);
+            Array.Copy(fileContents, 6, searchRangeBin, 0, searchRangeBin.Length);
+            Array.Copy(fileContents, 8, entrySelectorBin, 0, entrySelectorBin.Length);
+            Array.Copy(fileContents, 10, rangeShiftBin, 0, rangeShiftBin.Length);
+
+            float version = Conversions.ByteArrayToFixed(versionBin);
+            ushort numTables = BitConverter.ToUInt16(numTablesBin, 0);
+            ushort searchRange = BitConverter.ToUInt16(searchRangeBin, 0);
+            ushort entrySelector = BitConverter.ToUInt16(entrySelectorBin, 0);
+            ushort rangeShift = BitConverter.ToUInt16(rangeShiftBin, 0);
+
+            Console.WriteLine("Byte Array To Short: 01111111 11111111 - " + Conversions.ByteArrayToShort(new byte[] { 0x7F, 0xFF }));
+            Console.WriteLine("Byte Array To Short: 11111111 11111111 - " + Conversions.ByteArrayToShort(new byte[] { 0xFF, 0xFF }));
+            Console.WriteLine("Byte Array To UShort: 01111111 11111111 - " + Conversions.ByteArrayToUnsignedShort(new byte[] { 0x7F, 0xFF }));
+            Console.WriteLine("Byte Array To UShort: 11111111 11111111 - " + Conversions.ByteArrayToUnsignedShort(new byte[] { 0xFF, 0xFF }));
+
+            int z = 0;
+        }
+    }
+}
